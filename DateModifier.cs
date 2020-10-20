f9422fb6c1a59b993262924489abe1d56a5f4535
valichev <valichev92@gmail.com>
2020-10-20 10:02:26 +0300

    Add files via upload

---
+++ DateModifier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public int difference { get; set; }
+
+        public void CalculateDifference(string firstDate, string secondDate)
+        {
+            int[] firstDateList = firstDate.Split().Select(int.Parse).ToArray();
+            int[] secondDateList = secondDate.Split().Select(int.Parse).ToArray();
+            int firstYear = firstDateList[0];
+            int firstMonth = firstDateList[1];
+            int firstDay = firstDateList[2];
+            int secondYear = secondDateList[0];
+            int secondMonth = secondDateList[1];
+            int secondDay = secondDateList[2];
+
+            DateTime firstDateObj = new DateTime(firstYear, firstMonth, firstDay);
+            DateTime secondDateObj = new DateTime(secondYear, secondMonth, secondDay);
+
+            difference = Math.Abs((secondDateObj.Date - firstDateObj.Date).Days);
+            Console.WriteLine(difference);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DateModifier dateObject = new DateModifier();
+
+            dateObject.CalculateDifference(Console.ReadLine(), Console.ReadLine());
+        }
+    }
+}
+
+//3.
+//int n = int.Parse(Console.ReadLine());
+//Family family = new Family();
+
+//            for (int i = 0; i<n; i++)
+//            {
+//                List<string> inputList = Console.ReadLine().Split().ToList();
+//family.AddMember(new Person(inputList[0], int.Parse(inputList[1])));
+//            }
+
+//            Person oldestPerson = family.GetOldestMember();
+
+//Console.WriteLine(oldestPerson.Name + " " + oldestPerson.Age);
+
+//4.
+//int n = int.Parse(Console.ReadLine());
+//List<Person> people = new List<Person>();
+
+//for (int i = 0; i < n; i++)
+//{
+//    List<string> inputList = Console.ReadLine().Split().ToList();
+//    people.Add(new Person(inputList[0], int.Parse(inputList[1])));
+//}
+
+//List<Person> peopleOver30 = new List<Person>();
+
+//peopleOver30 = people.OrderBy(x => x.Name).Where(y => y.Age > 30).ToList();
+
+//foreach (var item in peopleOver30)
+//{
+//    Console.WriteLine($"{item.Name} - {item.Age}");
+//}
